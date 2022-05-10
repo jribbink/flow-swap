dc271b60f95150e1abc4f1a03c6f448bb7ea495e
Jordan Ribbink <jribbink@telus.net>
2022-05-10 10:53:39 -0700

    Remove useless transactions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,11 @@ export default ({children}: LayoutProps) => {
         <TransactionsProvider>
             <div className="bg-success bg-opacity-25 d-flex flex-column" style={overflowStyle}>
                 <Header />
-                <main className="container p-4 flex-grow-1">{children}</main>
+                <div className="d-flex flex-row flex-grow-1">
+                    <div className="col"></div>
+                    <main className="container p-4">{children}</main>
+                    <div className="col text-right">hey</div>
+                </div>
                 <Footer></Footer>
             </div>
         </TransactionsProvider>
